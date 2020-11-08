30465b788a63da2108dfd7abdda4cc7fb024d2e6
Anton Pedan <caravellejetta@gmail.com>
2020-11-08 16:12:36 +0200

    1612

--- app/screens/GalleryScreen.js
+++ app/screens/GalleryScreen.js
@@ -1,20 +1,44 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import ListItem from "../components/ListItem";
+
+//<ListItem />;
+const renderItem = () => (
+  <ListItem
+  // id={item.id}
+  // thumbnailUrl={item.urls.small}
+  // regularUrl={item.urls.regular}
+  // authorName={item.user.name}
+  // navigate={this.goToImageScreen}
+  />
+);
 
 function GalleryScreen() {
   return (
-    <>
-      <ImageBackground
-        style={styles.background}
-        source={require("../assets/icon.png")}
-      ></ImageBackground>
-    </>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        // data={DATA}
+        // renderItem={renderItem}
+        // keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
 });
+
 export default GalleryScreen;
--- app/store/reducers/fetchImagesReducer.js
+++ app/store/reducers/fetchImagesReducer.js
@@ -1,3 +1,39 @@
-const initialState = {};
+import {
+  FETCH_IMAGES_REQUEST,
+  FETCH_IMAGES_SUCCESS,
+  FETCH_IMAGES_FAILURE,
+} from "../actions/fetchImagesAction";
 
-export const fetchImagesReducer = (state = initialState) => {};
+const initialState = {
+  isFetching: false,
+  apidata: [],
+  errorMassage: "",
+};
+
+export const fetchImagesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_IMAGES_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case FETCH_IMAGES_SUCCESS: {
+      return {
+        ...state,
+        isFetching: false,
+        apidata: action.payload,
+      };
+    }
+    case FETCH_IMAGES_FAILURE: {
+      return {
+        ...state,
+        isFetching: false,
+        errorMassage: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
